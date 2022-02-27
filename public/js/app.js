60e34babaa1d075d8eb835f7e82f42b268856e99
Joshua <joshua.soave@gmail.com>
2022-02-27 16:27:10 -0500

    added card meanings upright and rev

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,21 @@
+//module
 var app = angular.module("fortuneTeller", [])
 
+//controller
 app.controller('fortuneTellerCtrl', ['$http', '$scope', function($http, $scope){
-    $scope.dealCards = function () {
-        console.log('hi');
-        $http.get("https://rws-cards-api.herokuapp.com/api/v1/cards/random?n=3")
-            .then(function (response) { $scope.cards = response.data; });
-        var cards = $scope.cards;
-        console.log(cards);
+    $scope.dealCards = async function () {
+        $scope.cardsDealt = true
+        await $http.get("https://rws-cards-api.herokuapp.com/api/v1/cards/random?n=3")
+            .then(function (response) { $scope.cards = response.data.cards; });
+        console.log($scope.cards);
     }
+    $scope.checkInverted = () => {
+        let x = Math.floor(Math.random() * 2)
+        console.log(x)
+        if(x){
+            $scope.inverted = true
+        } else {
+            $scope.inverted = false
+        }
+  }
 }])
